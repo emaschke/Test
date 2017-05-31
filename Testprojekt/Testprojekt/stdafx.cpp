a5d355faadbab85bb22d3062b8ab773c1b582381
Jan Enno Maschke <j.maschke@tu-braunschweig.de>
2017-05-31 19:11:59 +0200

    Projekt erstellt

---
+++ Testprojekt/Testprojekt/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
+// Testprojekt.pch ist der vorkompilierte Header.
+// stdafx.obj enthält die vorkompilierten Typinformationen.
+
+#include "stdafx.h"
+
+
